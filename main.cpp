d72635d5968b5f54ba4991150034d49080e2a24f
Raghav Pillai <gr8raghavsubra101@gmail.com>
2021-03-25 19:55:08 -0500

    Fixes on queue functions

--- main.cpp
+++ main.cpp
@@ -10,36 +10,38 @@ struct Node{ // Node structure, int variable and pointer to next node
     Node *next;
 };
 
-void enQueue(int nextValue, Node **header){ // Integer to add to list, node pointer that points to head of list
-    struct Node *newNode = new Node; // Creates node
+void enQueue(int nextValue, Node *&header){ // Integer to add to list, node pointer that points to head of list
+    Node *newNode = new Node; // Creates node
     newNode->num = nextValue; // Assigns node to back of list
-    newNode->next = nullptr; // Temporarily assigns node no next value
+    newNode->next = NULL; // Temporarily assigns node no next value
 
-    if(*header == nullptr){ // If previous node doesn't exist
-        *header = newNode;
+    if(header == NULL){ // If previous node doesn't exist
+        header = newNode;
     }else{ // If header node exists
-        struct Node *current = *header;
-
-        while(current->next != nullptr){ 
-            current = current->next;
+        Node *temp = header;
+        while(temp->next != NULL){ 
+            temp = temp->next;
         }
-        current->next = newNode;// Puts node all the way in the back
+        temp->next = newNode; // Puts node all the way in the back
     }
 }
 
-void deQueue(Node **header){
-    if(header == nullptr){ // If header doesn't exist
-        return;
-    }
+void deQueue(Node *&header){ // Removes item from queue
+    if(header->next == NULL){ // If no next node
+        header = header->next;
+    }else{ // If node exists
+        Node *temp = header;
 
-    Node *tempNum = *header; // New temporary header
-    *header = tempNum->next; // Removes the number from list
-    
-    delete tempNum; // Deletes the temporary number
+        while(temp->next->next != NULL){ // Checks for each variable
+            temp = temp->next;
+        }
+
+        temp->next = NULL;
+   }
 }
 
 void printQueue(Node *header) { // Function to print items, input header node
-    if(header != nullptr) { // If header doesn't exist
+    if(header != NULL) { // If header doesn't exist
         cout << header->num << " ";
         printQueue(header->next); // Prints the next header
     }else{
@@ -48,15 +50,33 @@ void printQueue(Node *header) { // Function to print items, input header node
 }
 
 int main(){
-    struct Node *head = nullptr;
+    Node *head = NULL;
 
-    enQueue(1, &head);
+    enQueue(2, head);
     printQueue(head);
 
-    enQueue(2, &head);
+    enQueue(5, head);
     printQueue(head);
 
-    deQueue(&head);
+    enQueue(1, head);
+    printQueue(head);
+
+    enQueue(5, head);
+    printQueue(head);
+
+    enQueue(1, head);
+    printQueue(head);
+
+    deQueue(head);
+    printQueue(head);
+
+    deQueue(head);
+    printQueue(head);
+
+    enQueue(3, head);
+    printQueue(head);
+
+    enQueue(2, head);
     printQueue(head);
 
     return 0;
